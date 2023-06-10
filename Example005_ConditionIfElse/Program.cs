b4593bb8a5221925cb584d1d7188b19e151ee5dd
Saykhan <info@saykhan.ru>
2023-06-10 23:35:38 +0300

    Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "сайхан")
+{
+    Console.WriteLine("Марша вохийла, Сайхан!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.Write("!");
+}
