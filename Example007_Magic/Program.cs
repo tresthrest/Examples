64f131437ad9fa7a593d87c27728e2422b928087
Saykhan <info@saykhan.ru>
2023-06-11 10:51:41 +0300

    Copy

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -14,33 +14,3 @@ Console.WriteLine("+");
 
 Console.SetCursorPosition(xc, yb);
 Console.WriteLine("+");
-
-int x = xa, y = xb;
-
-int count = 0;
-
-while (count < 10)
-{
-    int what = new Random().Next(0, 3); // [0;3) 0 1 2
-    if (what == 0)
-    {
-        x = (x + xa) / 2;
-        y = (y + ya) / 2;
-    }
-
-    if (what == 1)
-    {
-        x = (x + xb) / 2;
-        y = (y + yb) / 2;
-    }
-
-    if (what == 2)
-    {
-        x = (x + xc) / 2;
-        y = (y + yc) / 2;
-    }
-
-    Console.SetCursorPosition(x, y);
-    Console.WriteLine("+");
-    count++; 
-}
